6850d611f0c7fea61275ad0827bbaeacd7f28d12
pui97 <81636308+pui97@users.noreply.github.com>
2021-04-07 15:30:22 +0800

    Add files via upload

---
+++ pyunit.py
@@ -0,0 +1,30 @@
+import unittest
+import test
+
+class CalculatorTestCase(unittest.TestCase):
+
+
+    def test_aaron(self):
+        
+        result = test.single_calculate(600000 )
+        self.assertEqual(result, 58500)
+
+        
+    def test_aaron1(self):
+        
+        result = test.married_single_calculate(600000 ,400000 )
+        self.assertEqual(result, 83000)
+
+    def test_aaron2(self):
+        
+        result = test.married_calculate(600000 ,400000 )
+        self.assertEqual(result, 101000)
+
+    def test_aaron3(self):
+        
+        result = test.suggestion(83000,101000)
+        self.assertEqual(result, "suggest use single Assesment")
+
+
+if __name__ == '__main__':
+    unittest.main()
